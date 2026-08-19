9e08a2462bb704ff2eb887164d29fd4740dcdaaf
Tu Nombre <cdmartinez1121@gmail.com>
2026-08-19 13:37:04 -0500

    Prueba para CI/CD

--- sample_app.py
+++ sample_app.py
@@ -8,7 +8,7 @@ def home():
     try:
         conn = pymysql.connect(host='servidor-bd-ejemplo', user='root', password='sena123', database='082_db')
         conn.close()
-        db_status = "Conexión exitosa a la base de datos"
+        db_status = "Conexión exitosa a la base de datos, prueba para CI/CD"
     except Exception as e:
         db_status = f"Error al conectar a la base de datos: {e}"
 
